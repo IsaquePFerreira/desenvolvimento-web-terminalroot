096fb746d3c9455b05aaeacb89ecd98da153ebe5
IsaquePFerreira <pferreiraisaque4@gmail.com>
2022-06-17 19:33:40 -0300

    modify files document.html and script.js

--- exercicios/javascript/script.js
+++ exercicios/javascript/script.js
@@ -1,4 +1,61 @@
 //var x = "Ei nerd";
+
 /*var y = "Traks?!...";
 alert(x + ", " + y);*/
+
 //alert("Do js")
+
+// + - * / %
+/*var x = 3;
+var y = 6;
+
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
+alert(x % y);
+*/
+
+// arrays
+//var z = ["Isaque", "Bravo"];
+
+//alert(z[1]);
+
+/*var z = [1, 4, 6];
+
+alert(z[0] + z[1] + z[2]);
+
+var x = 4 * ((2 + 1) * 2)
+alert(x)*/
+
+/*function MyFunction(x, y) {
+	return x + y;
+}*/
+
+//var x;
+
+/*if (x >= 8) {
+	alert ("Sim, é maior ou igual a oito!");
+} else if (x >= 7) {
+	alert ("É maior ou igual a sete!");
+} else {
+	alert ("Não, é menor do que sete!");
+}
+*/
+
+/*while (x < 21) {
+	alert(x);
+	x++;
+}*/
+
+/*for ( x = 0; x < 6; x++) {
+	alert(x);
+}*/
+
+var carro = {
+	modelo: "Golf",
+	ano: 2020,
+	cor: "Azul"
+}
+
+alert("Ano:" + carro.ano + "\n" + "Cor" + carro.cor)
