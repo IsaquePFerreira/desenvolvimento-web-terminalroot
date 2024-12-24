9e3f29dc8af6b7769a7b4a1c979fa4e470d5b674
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-12-24 10:50:29 -0300

    fix: Mini JS review

--- exercicios/javascript/script.js
+++ exercicios/javascript/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //var x = "Ei nerd";
 
 /*var y = "Traks?!...";
@@ -59,3 +60,97 @@ var carro = {
 }
 
 alert("Ano:" + carro.ano + "\n" + "Cor" + carro.cor)
+=======
+/* 
+    NOMES DE VARIÁVEIS
+    Não comecao com número.
+    Não utilize espaços para variáveis com mais de um nome.
+    Não utilize caracteres especiais(ç,~,^, ...).
+    Use nomes com sentido.
+*/
+
+// var tem problema de vazamento de escopo...
+// var ornitorrinco = "Perry";
+// let alternativa moderna sem vazamento de escopo...
+// let saudacao = "Traks?!..."
+// const canalPeter = "Ei nerd";
+
+// const não pode ser reatribuida...
+// canalPeter = "outra coisa";
+
+// Exibi uma janela de alerta no browser...
+// alert(canalPeter + ", " + saudacao);
+
+// Capturando um elemento do DOM...
+// const quadro = document.querySelector("#quadro");
+// Exibindo algo no console...
+// console.log("[quadro]", quadro);
+
+// Aritmética...
+// let primeiro = 3;
+// let segundo = 6;
+
+// let soma = primeiro + segundo;
+// let subtracao = primeiro - segundo;
+// let multiplicacao = primeiro * segundo;
+// let divisao = primeiro / segundo;
+// let resto = primeiro & segundo;
+
+// Adicionando items a um elemento...
+// quadro.append("Soma: ", soma);
+// quadro.append(" ");
+// quadro.append("Subtração: ", subtracao);
+// quadro.append(" ");
+// quadro.append("Multiplicação: ", multiplicacao);
+// quadro.append(" ");
+// quadro.append("Divisão: ", divisao);
+// quadro.append(" ");
+// quadro.append("Resto da divisão: ", resto);
+
+// arrays
+// const palavras = ["Isaque", "Bravo"];
+// 
+// console.log(palavras[1]);
+// 
+// const numeros = [1, 4, 6];
+// 
+// console.log(numeros[0] + numeros[1] + numeros[2]);
+
+// functions
+
+// let first = 4 * ((2 + 1) * 2);
+// // console.log(first);
+// 
+// function sum(first, second) {
+// 	return first + second;
+// }
+// 
+// console.log(sum(first, 5));
+
+// var x = 1;
+
+// if (x >= 8) {
+// 	console.log("Sim, é maior ou igual a oito!");
+// } else if (x >= 7) {
+// 	console.log("É maior ou igual a sete!");
+// } else {
+// 	console.log("Não, é menor do que sete!");
+// }
+
+// while (x < 21) {
+// 	console.log(x);
+// 	x++;
+// }
+
+// for ( x = 0; x < 6; x++) {
+// 	console.log(x);
+// }
+
+:x
+//     modelo: "Golf",
+//     ano: 2020,
+//     cor: "Azul"
+// }
+// 
+// console.log("Modelo: " + carro.modelo + "\n" + "Ano: " + carro.ano + "\n" + "Cor: " + carro.cor);
+>>>>>>> 326d15e (fix remote)
