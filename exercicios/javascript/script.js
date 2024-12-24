456a9d8a35db515d52184dfc1c265b3add66ae58
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-12-24 10:53:06 -0300

    fix: Mini JS review - part I

--- exercicios/javascript/script.js
+++ exercicios/javascript/script.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-//var x = "Ei nerd";
-
-/*var y = "Traks?!...";
-alert(x + ", " + y);*/
-
-//alert("Do js")
-
-// + - * / %
-/*var x = 3;
-var y = 6;
-
-alert(x + y);
-alert(x - y);
-alert(x * y);
-alert(x / y);
-alert(x % y);
-*/
-
-// arrays
-//var z = ["Isaque", "Bravo"];
-
-//alert(z[1]);
-
-/*var z = [1, 4, 6];
-
-alert(z[0] + z[1] + z[2]);
-
-var x = 4 * ((2 + 1) * 2)
-alert(x)*/
-
-/*function MyFunction(x, y) {
-	return x + y;
-}*/
-
-//var x;
-
-/*if (x >= 8) {
-	alert ("Sim, é maior ou igual a oito!");
-} else if (x >= 7) {
-	alert ("É maior ou igual a sete!");
-} else {
-	alert ("Não, é menor do que sete!");
-}
-*/
-
-/*while (x < 21) {
-	alert(x);
-	x++;
-}*/
-
-/*for ( x = 0; x < 6; x++) {
-	alert(x);
-}*/
-
-var carro = {
-	modelo: "Golf",
-	ano: 2020,
-	cor: "Azul"
-}
-
-alert("Ano:" + carro.ano + "\n" + "Cor" + carro.cor)
-=======
 /* 
     NOMES DE VARIÁVEIS
     Não comecao com número.
@@ -146,11 +83,9 @@ alert("Ano:" + carro.ano + "\n" + "Cor" + carro.cor)
 // 	console.log(x);
 // }
 
-:x
 //     modelo: "Golf",
 //     ano: 2020,
 //     cor: "Azul"
 // }
 // 
 // console.log("Modelo: " + carro.modelo + "\n" + "Ano: " + carro.ano + "\n" + "Cor: " + carro.cor);
->>>>>>> 326d15e (fix remote)
